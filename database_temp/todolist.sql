1809c3b7aa1b50c93fdf8a9dd3b3f34fcd01e676
Danny LNX <dannybrenio060@gmail.com>
2026-04-13 13:39:10 +0800

    changes to the notesql

--- database_temp/todolist.sql
+++ database_temp/todolist.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Apr 09, 2026 at 01:04 AM
+-- Host: localhost
+-- Generation Time: Apr 13, 2026 at 07:37 AM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.3.26
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -30,19 +30,12 @@ SET time_zone = "+00:00";
 CREATE TABLE `notes` (
   `note_id` bigint(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `note` varchar(300) NOT NULL,
+  `note_title` varchar(255) DEFAULT NULL,
+  `note_body` varchar(255) DEFAULT NULL,
   `status` tinyint(4) NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `notes`
---
-
-INSERT INTO `notes` (`note_id`, `user_id`, `note`, `status`, `created_at`, `updated_at`) VALUES
-(1, 1, 'notesample', 1, '2026-03-10 01:14:10', '2026-03-10 01:14:23'),
-(2, 1, 'sampletwo', 1, '2026-03-10 01:59:50', '2026-03-10 01:59:59');
 
 -- --------------------------------------------------------
 
@@ -63,7 +56,13 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `username`, `password`, `created_at`, `updated_at`) VALUES
-(1, 'danny', 'danny', '2026-03-10 01:13:57', '2026-03-10 01:14:04');
+(1, 'danny', 'danny', '2026-03-10 01:13:57', '2026-03-10 01:14:04'),
+(2, 'danny1', '$2a$10$ri4WuLKFT7Jtoc2Fyy/l8uvN6Zn.cQct4SiHV/.t/8tOs7RLWV9HO', '2026-04-10 10:04:02', '2026-04-10 02:04:02'),
+(3, 'danny0', '$2a$10$e2MmnpT1DjJMAh5iF548ceONMLiQC5P23IQ63rLg5RYOYajp0/WL6', '2026-04-10 10:06:55', '2026-04-10 02:06:55'),
+(4, 'soyii', '$2a$10$oxCnUAXwc5nwNVFuEnHf0uDlbEq6k0l3i781QKpDddU2PFxjo/mTu', '2026-04-10 14:06:27', '2026-04-10 06:06:27'),
+(5, 'dannybrens', '$2a$10$rBkC9F4RWwETEdrf6LDDVuoGBs5Lg9IOOTPFjsT2tfWjLP5Bj0Qbm', '2026-04-13 09:12:31', '2026-04-13 01:12:31'),
+(6, 'danny2', '$2a$10$GSjXq3NuGRHAmnNpD8rGsesNOSRW3OCUsh4hpi8D5GvvP9gbVzPXO', '2026-04-13 09:40:42', '2026-04-13 01:40:42'),
+(7, 'noela', '$2a$10$9NIeLdGCtPgjpv6UQQpUKeqLsJ4tEhUdZ20FSRVLyH6wwaljUSf2y', '2026-04-13 09:54:37', '2026-04-13 01:54:37');
 
 --
 -- Indexes for dumped tables
@@ -96,7 +95,7 @@ ALTER TABLE `notes`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Constraints for dumped tables
